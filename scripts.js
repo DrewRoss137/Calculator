0d7d14e94f781efe5f0b816fe44ad2259977d5fc
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-03-16 21:41:25 +0000

    Create core calculator functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let currentInput = "";
+let previousInput = "";
+let currentOperator = "";
+
 const display = document.getElementById("display");
 
 const buttons = document.getElementById("buttons");
@@ -5,32 +9,25 @@ buttons.addEventListener("click", (event) => {
     const target = event.target;
     const isDigitButton = target.classList.contains("digit-buttons");
     const isOperatorButton = target.classList.contains("operator-buttons");
-    if (isDigitButton || isOperatorButton) {
-        if (isOperatorButton) {
-            const lastChar = display.textContent.slice(-1);
-            if (isOperator(lastChar)) {
-                backspace();
-            }
+    if (isDigitButton) {
+        currentInput += target.textContent;
+        display.textContent += target.textContent;
+    } else if (isOperatorButton) {
+        if (previousInput && currentOperator && currentInput) {
+            previousInput = evaluateExpression(previousInput, currentInput, currentOperator);
+            display.textContent = roundResult(previousInput);
+            currentInput = "";
+        } else if (currentInput) {
+            previousInput = currentInput;
+            currentInput = "";
         }
-        appendDisplay(target.textContent);
+        if (isOperator(display.textContent.slice(-1))) {
+            backspace();
+        }
+        display.textContent += target.textContent;
+        currentOperator = target.textContent;
     }
 });
-
-const exponentButton = document.getElementById("exponent-button");
-
-const factorialButton = document.getElementById("factorial-button");
-
-const percentButton = document.getElementById("percent-button");
-
-const colourButton = document.getElementById("colour-button");
-
-const powerButton = document.getElementById("power-button");
-
-const sevenButton = document.getElementById("seven-button");
-
-const eightButton = document.getElementById("eight-button");
-
-const nineButton = document.getElementById("nine-button");
 
 const deleteButton = document.getElementById("delete-button");
 deleteButton.addEventListener("click", () => {
@@ -42,49 +39,56 @@ allClearButton.addEventListener("click", () => {
     clearDisplay();
 });  
 
-const fourButton = document.getElementById("four-button");
-
-const fiveButton = document.getElementById("five-button");
-
-const sixButton = document.getElementById("six-button");
-
-const multiplicationButton = document.getElementById("multiplication-button");
-
-const divisionButton = document.getElementById("division-button");
-
-const oneButton = document.getElementById("one-button");
-
-const twoButton = document.getElementById("two-button");
-
-const threeButton = document.getElementById("three-button");
-
-const additionButton = document.getElementById("addition-button");
-
-const subtractionButton = document.getElementById("subtraction-button");
-
-const zeroButton = document.getElementById("zero-button");
-
-const decimalPointButton = document.getElementById("decimal-point-button");
-
-const signButton = document.getElementById("sign-button");
-
-const answerButton = document.getElementById("answer-button");
-
 const equalsButton = document.getElementById("equals-button");
+equalsButton.addEventListener("click", () => {
+    if (previousInput && currentOperator && currentInput) {
+        const result = evaluateExpression(previousInput, currentInput, currentOperator);
+        display.textContent = roundResult(result);
+        currentInput = result.toString();
+        previousInput = "";
+        currentOperator = "";
+    }
+});
 
 function appendDisplay(value) {
     display.textContent += value;
-}
+};
 
 function backspace() {
     display.textContent = display.textContent.slice(0, -1);
-}
+};
 
 function clearDisplay() {
     display.textContent = "";
-}
+    currentInput = "";
+    previousInput = "";
+    currentOperator = "";
+};
 
 function isOperator(char) {
-    const operators = ['+', '–', 'x', '÷'];
+    const operators = ["+", "–", "x", "÷"];
     return operators.includes(char);
-}
+};
+
+function evaluateExpression(a, b, operator) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch (operator) {
+        case "+":
+            return a + b;
+        case "–":
+            return a - b;
+        case "x":
+            return a * b;
+        case "÷":
+            if (b === 0) {
+                display.textContent = "Can't divide by 0!";
+                return a;
+            }
+            return a / b;
+    }
+};
+
+function roundResult(value) {
+    return Math.round(value * 1000000) / 1000000;
+};
