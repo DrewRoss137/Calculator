c31aa088ba178a17399e75395030651bfa5c19e1
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-03-18 14:42:10 +0000

    Implement colour toggle functionality

--- scripts.js
+++ scripts.js
@@ -1,304 +1,59 @@
-let currentInput = "";
-let previousInput = "";
-let currentOperator = "";
-let lastResult = "";
-let lastButtonModulo = false;
+const body = document.body;
+
+const calculator = document.getElementById("calculator");
 
 const display = document.getElementById("display");
-let displayFactorial = false;
-buttons.addEventListener("click", (event) => {
-    if (powerButton.classList.contains("off")) {
-        display.textContent = "";
-        currentInput = "";
-        previousInput = "";
-        currentOperator = "";
-        return;
-    }
 
-    const target = event.target;
-    const isDigitButton = target.classList.contains("digit-buttons");
-    const isOperatorButton = target.classList.contains("operator-buttons");
+const buttons = document.getElementById("buttons");
 
-    if (isDigitButton || target === decimalPointButton) {
-        if (target === decimalPointButton && currentInput.includes(".")) {
-            return;
-        }
-        currentInput += target.textContent;
-        display.textContent += target.textContent;
-    } else if (isOperatorButton) {
-        if (lastButtonModulo) {
-            const result = parseFloat(previousInput) / 100;
-            currentInput = result.toString();
-            display.textContent = roundResult(result) + target.textContent;
-            previousInput = currentInput;
-            currentInput = "";
-            lastButtonModulo = false;
-        } else if (displayFactorial) {
-            const result = evaluateExpression(previousInput, currentOperator);
-            currentInput = result.toString();
-            display.textContent = roundResult(result);
-            displayFactorial = false;
-        }
+const miscellaneousButtons = document.querySelectorAll(".miscellaneous-buttons");
 
-        if (previousInput && currentOperator && currentInput) {
-            previousInput = evaluateExpression(previousInput, currentOperator, currentInput);
-            display.textContent = roundResult(previousInput);
-            currentInput = "";
-        } else if (currentInput) {
-            previousInput = currentInput;
-            currentInput = "";
-        }
-        if (isOperator(display.textContent.slice(-1))) {
-            backspace();
-        }
-        display.textContent += target.textContent;
-        currentOperator = target.textContent;
-    }
-
-});
-
-
+const digitButtons = document.querySelectorAll(".digit-buttons");
 
 const exponentButton = document.getElementById("exponent-button");
-exponentButton.addEventListener("click", () => {
-    if (currentInput) {
-        currentOperator = "^";
-        previousInput = currentInput;
-        currentInput = "";
-        display.textContent += "^";
-    }
-});
-
 
 const factorialButton = document.getElementById("factorial-button");
-factorialButton.addEventListener("click", () => {
-    if (currentInput) {
-        currentOperator = "!";
-        previousInput = currentInput;
-        currentInput = "";
-        display.textContent += "!";
-        displayFactorial = true;
-    }
-});
 
 const moduloButton = document.getElementById("modulo-button");
-moduloButton.addEventListener("click", () => {
-    if (currentInput) {
-        if (isOperator(display.textContent.slice(-1))) {
-            backspace();
-        }
-        lastButtonModulo = true;
-        currentOperator = "%";
-        previousInput = currentInput;
-        currentInput = "";
-        display.textContent += "%";
-    }
-});
-
-
-
 
 const colourButton = document.getElementById("colour-button");
-colourButton.addEventListener("click", () => {
-    toggleCalculatorColor();
-});
+colourButton.addEventListener("click", toggleColourMode);
 
 const powerButton = document.getElementById("power-button");
-powerButton.addEventListener("click", () => {
-    togglePowerButtonStyle();
-});
 
 const deleteButton = document.getElementById("delete-button");
-deleteButton.addEventListener("click", backspace);
 
 const allClearButton = document.getElementById("all-clear-button");
-allClearButton.addEventListener("click", clearDisplay);
+
+const multiplicationButton = document.getElementById("multiplication-button");
+
+const divisionButton = document.getElementById("division-button");
+
+const additionButton = document.getElementById("addition-button");
+
+const subtractionButton = document.getElementById("subtraction-button");
 
 const decimalPointButton = document.getElementById("decimal-point-button");
-decimalPointButton.addEventListener("click", () => {
-    if (!currentInput.includes(".")) {
-        currentInput += ".";
-        display.textContent += ".";
-    }
-});
 
 const signButton = document.getElementById("sign-button");
-signButton.addEventListener("click", toggleSign);
 
-let answerVisible = false;
 const answerButton = document.getElementById("answer-button");
-answerButton.addEventListener("click", toggleAnswer);
-function toggleAnswer() {
-    if (!answerVisible) {
-        if (lastResult !== null) {
-            display.textContent = lastResult.toString();
-            answerVisible = true;
-        }
-    } else {
-        updateDisplay();
-        answerVisible = false;
-    }
-}
 
 const equalsButton = document.getElementById("equals-button");
-equalsButton.addEventListener("click", () => {
-    if (displayFactorial) {
-        const result = evaluateExpression(previousInput, currentOperator);
-        currentInput = result.toString();
-        display.textContent = roundResult(result);
-        displayFactorial = false;
-        currentOperator = "";
-    } else if (lastButtonModulo) {
-        const result = parseFloat(previousInput) % parseFloat(currentInput);
-        display.textContent = roundResult(result);
-        currentInput = result.toString();
-        previousInput = "";
-        currentOperator = "";
-        lastResult = result;
-        lastButtonModulo = false;
-    } else if (previousInput && currentOperator && currentInput) {
-        const result = evaluateExpression(previousInput, currentOperator, currentInput);
-        display.textContent = roundResult(result);
-        currentInput = result.toString();
-        previousInput = "";
-        currentOperator = "";
-        lastResult = result;
-    }
-});
 
+let darkMode = false;
 
-function backspace() {
-    display.textContent = display.textContent.slice(0, -1);
-};
+toggleColourMode()
 
-function clearDisplay() {
-    display.textContent = "";
-    currentInput = "";
-    previousInput = "";
-    currentOperator = "";
-};
-
-function isOperator(char) {
-    const operators = ["+", "–", "x", "÷", "!", "%"];
-    return operators.includes(char);
-};
-
-
-function roundResult(value) {
-    return Math.round(value * 1000000) / 1000000;
-};
-
-function toggleSign() {
-    if (currentInput === "") {
-        currentInput = "-";
-        display.textContent += "-";
-    } else if (currentInput === "-") {
-        currentInput = "";
-        backspace();
-    } else {
-        currentInput = (parseFloat(currentInput) * -1).toString();
-        updateDisplay();
-    }
-};
-
-function updateDisplay() {
-    if (previousInput && currentOperator) {
-        display.textContent = previousInput + currentOperator + currentInput;
-    } else if (previousInput) {
-        display.textContent = previousInput;
-    } else {
-        display.textContent = currentInput;
-    }
-};
-
-function evaluateExpression(a, operator, b = null) {
-    a = parseFloat(a);
-    if (b !== null) b = parseFloat(b);
-    if (operator === "+") return a + b;
-    if (operator === "–") return a - b;
-    if (operator === "x") return a * b;
-    if (operator === "÷") return (b === 0) ? a : a / b;
-    if (operator === "^") return Math.pow(a, b);
-    if (operator === "!") return factorial(a);
-    if (operator === "%") return a % b;
-}
-
-
-
-
-function factorial(n) {
-    if (n === 0 || n === 1) return 1;
-    return n * factorial(n - 1);
-};
-
-function toggleCalculatorColor() {
-    const calculator = document.querySelector(".calculator");
-    const buttons = document.querySelectorAll(".digit-buttons");
-    const colorButton = document.querySelector("#colour-button");
-    const display = document.querySelector("#display");
-
-    calculator.classList.toggle("light");
-    display.classList.toggle("light");
-
-    if (calculator.classList.contains("light")) {
-        buttons.forEach(button => {
-            button.classList.remove("dark-theme-button");
-            button.classList.add("light-theme-button");
-        });
-        colorButton.classList.remove("dark-theme-button");
-        colorButton.classList.add("light-theme-button");
-        colorButton.classList.remove("dark-theme-color-button");
-        colorButton.classList.add("light-theme-color-button");
-
-        display.classList.remove("dark-display");
-        display.classList.add("light-display");
-    } else {
-        buttons.forEach(button => {
-            button.classList.remove("light-theme-button");
-            button.classList.add("dark-theme-button");
-        });
-        colorButton.classList.remove("light-theme-button");
-        colorButton.classList.add("dark-theme-button");
-        colorButton.classList.remove("light-theme-color-button");
-        colorButton.classList.add("dark-theme-color-button");
-
-        display.classList.remove("light-display");
-        display.classList.add("dark-display");
-    }
-}
-
-  
-function togglePowerButtonStyle() {
-    const powerButton = document.getElementById("power-button");
-    if (powerButton.classList.contains("on")) {
-        powerButton.classList.remove("on");
-        powerButton.classList.add("off");
-    } else {
-        powerButton.classList.remove("off");
-        powerButton.classList.add("on");
-    }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    setPowerButtonOn();
-    setDefaultDarkMode();
-});
-
-function setPowerButtonOn() {
-    const powerButton = document.getElementById("power-button");
-    powerButton.classList.add("on");
-}
-function setDefaultDarkMode() {
-    const buttons = document.querySelectorAll(".digit-buttons");
-    const colorButton = document.querySelector("#colour-button");
-  
-    buttons.forEach(button => {
-      button.classList.add("dark-theme-button");
+function toggleColourMode() {
+    darkMode = !darkMode;
+    [calculator, display, ...miscellaneousButtons, ...digitButtons].forEach(element => {
+      toggleClass(element, darkMode);
     });
+};
   
-    colorButton.classList.add("dark-theme-button");
-    colorButton.classList.add("dark-theme-color-button");
-  }
-  
-  
+function toggleClass(element, darkMode) {
+    element.classList.toggle("dark-mode", darkMode);
+    element.classList.toggle("light-mode", !darkMode);
+};
   
